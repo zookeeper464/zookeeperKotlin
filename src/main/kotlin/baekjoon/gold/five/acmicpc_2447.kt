32f70d92d363c35355ff0e291253e2efbd6fb2ad
김하진 <78637227+zookeeper464@users.noreply.github.com>
2023-06-15 00:14:13 +0900

    draw Star 2447
    재귀함수를 활용하여 시간이 별그림을 그렸습니다.

--- src/main/kotlin/baekjoon/gold/five/acmicpc_2447.kt
+++ src/main/kotlin/baekjoon/gold/five/acmicpc_2447.kt
@@ -1,8 +1,16 @@
 package baekjoon.gold.five
 
-fun acmicpc_2447(N: Int): String {
+fun drawStar(): String {
+    val N = readln().toInt()
+    fun recursiveStar (N:Int): List<List<String>> {
+        if (N == 3) return listOf(listOf("*","*","*"),listOf("*"," ","*"),listOf("*","*","*"))
+
+        return recursiveStar(N/3).map{ it+it+it. } + recursiveStar(N/3).map { it+List(N/3){" "}+it } + recursiveStar(N/3).map { it+it+it }
+    }
+    val answerList = recursiveStar(N)
+    return answerList.joinToString("\n") { it.joinToString("") }
 }
 
 fun main () {
-
+    println(drawStar())
 }
--- src/main/kotlin/baekjoon/gold/five/acmicpc_7576.kt
+++ src/main/kotlin/baekjoon/gold/five/acmicpc_7576.kt
@@ -3,7 +3,7 @@ package baekjoon.gold.five
 import baekjoon.common.inputLines
 import baekjoon.common.moveDoubleList
 
-fun tomato () {
+fun tomato (): Int {
     var answer = -1
     val (M, N) = readln().split(" ").map { it.toInt() }
     val tomatoMap = inputLines(N).map { it.split(" ").map { it.toInt() }.toMutableList() }.toMutableList()
@@ -19,13 +19,9 @@ fun tomato () {
         tomatoQueue = moveDoubleList(M,N,tomatoQueue,tomatoMap)
         answer++
     }
-    if (tomatoMap.flatten().contains(0)) {
-        println(-1)
-    } else {
-        println(answer)
-    }
+    return if (tomatoMap.flatten().contains(0)) -1 else answer
 }
 
 fun main () {
-    tomato()
+    println(tomato())
 }
