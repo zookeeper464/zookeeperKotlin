da4374152e94d6dce4a521c6d73b49882ce18a31
김하진 <78637227+zookeeper464@users.noreply.github.com>
2023-06-22 22:33:04 +0900

    reset code
    코드 변환이 있었지만 다시 돌렸습니다.

--- src/main/kotlin/baekjoon/gold/five/acmicpc_2447.kt
+++ src/main/kotlin/baekjoon/gold/five/acmicpc_2447.kt
@@ -5,7 +5,7 @@ fun drawStar(): String {
     fun recursiveStar (N:Int): List<List<String>> {
         if (N == 3) return listOf(listOf("*","*","*"),listOf("*"," ","*"),listOf("*","*","*"))
 
-        return recursiveStar(N/3).map{ it+it+it. } + recursiveStar(N/3).map { it+List(N/3){" "}+it } + recursiveStar(N/3).map { it+it+it }
+        return recursiveStar(N/3).map{ it+it+it } + recursiveStar(N/3).map { it+List(N/3){" "}+it } + recursiveStar(N/3).map { it+it+it }
     }
     val answerList = recursiveStar(N)
     return answerList.joinToString("\n") { it.joinToString("") }
